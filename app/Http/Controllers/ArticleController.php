5b8495d029715a7a9a59fb52bf5ecdc4756d5856
418: I_am_Caleb <ebukauche52@gmail.com>
2025-10-31 17:13:27 +0100

    feat: filter articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,13 +13,39 @@ class ArticleController extends Controller
     /**
      * Fetch Articles
      * 
-     * @param Request $request
      * @return JsonResponse
      * @throws \Illuminate\Validation\ValidationException
      */
     public function index(Request $request): JsonResponse
-    {     
-        $data = Article::find();
+    {
+        $request->validate([
+            'category' => 'nullable|string',
+            'date' => 'nullable|date',
+            'source' => 'nullable|string|in:NewsApiAi,NewsApiOrg,TheGuardian',
+            // the total number of items to be fetched per page, default is 10
+            'pageSize' => 'nullable|integer|min:1|max:100',
+            // the page number to be fetched, default is 1
+            'page' => 'nullable|integer|min:1',
+        ]);
+
+        $query = Article::query();
+
+        if ($request->has('date')) {
+            $query->whereDate('published_at', $request->date);
+        }
+
+        if ($request->has('category')) {
+            $query->where('category', $request->category);
+        }
+
+        if ($request->has('source')) {
+            $query->where('source', $request->source);
+        }
+        
+        $perPage = $request->input('pageSize', 10);
+        $page = $request->input('page', 1);
+        $data = $query->orderByDesc('published_at')->paginate($perPage, ['*'], 'page', $page);
+
 
         return response()->json([
             'status' => 'success',
